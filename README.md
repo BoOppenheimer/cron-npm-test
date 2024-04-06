bc6b45ae5c5f2f86d60e18984674123b7bb93fe8
Bo <bo.opfer@mara.com>
2024-04-06 15:06:55 -0400

    1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# A simple test for vercel to see how this thing works
+
+NPM package Cron vs system cron well we keep the state with node baby.
+
+NPM - [Cron](https://www.npmjs.com/package/cron)
---
+++ index.mjs
@@ -0,0 +1,16 @@
+import { CronJob } from 'cron';
+
+const job = new CronJob(
+	'1 * * * * *', // cronTime
+	function () {
+		console.log('You will see this message every minute');
+		console.log(Date(Date.now()).toString());
+
+	}, // onTick
+	null, // onComplete
+	true, // start
+	'America/Los_Angeles' // timeZone
+);
+
+console.log("some love?")
+// job.start() is optional here because of the fourth parameter set to true.
