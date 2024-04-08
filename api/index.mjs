5e9e46a5a476f4914724c0afe1213c9a8c0638dd
Bo Opfer <booppenheimer@gmail.com>
2024-04-08 10:22:16 -0400

    update simple express, cron

--- api/index.mjs
+++ api/index.mjs
@@ -1,22 +1,63 @@
 import { CronJob } from 'cron';
+import { DateTime } from 'luxon';
+import express from 'express';
 
-const job = new CronJob(
-	'1 * * * * *', // cronTime
+
+const app = express();
+const PORT = process.env.PORT || 3000; //vercel will define or 3000
+const startOfTodayUTC = DateTime.utc().startOf('day');
+
+console.log("this is now:" ,startOfTodayUTC);
+
+let myCountMin = 0;
+let dailyCountTotal = 0;
+console.log("---NODE CRON RUNNING-----");
+
+const jobA = new CronJob(
+	'0 * * * * *', // cronTime every mins setting sec to 0
 	function () {
-		console.log('You will see this message every minute');
-		console.log(Date(Date.now()).toString());
+		myCountMin++
+		checkBlockAward();
+		
 
 	}, // onTick
 	null, // onComplete
 	true, // start
-	'America/Los_Angeles' // timeZone
+	'UTC' // timeZone
 );
 
+const jobB = new CronJob(
+	'0 */5 * * * *', // cronTime every mins setting sec to 0
+	function () {
+		dailyCountTotal++
+		dailySummery();
+		
 
-// job.start() is optional here because of the fourth parameter set to true.
+	}, // onTick
+	null, // onComplete
+	true, // start
+	'UTC' // timeZone
+);
 
-
-module.exports = (req, res) => {
-	console.log("some love?")
-	res.status(200).json ({ message: "hello from mars"});
+function checkBlockAward () {
+	console.log(`Every Min: ${Date.now().toString()}`);
+	console.log(`Min count Total: ${myCountMin}`);
+	
 }
+
+function dailySummery () {
+	console.log(`Every 5 Min: ${Date.now().toString()}`);
+	console.log(`5 Min count Total: ${dailyCountTotal}`);
+}
+
+app.get('/', function (req, res) {
+  res.send(`- Ready sir! ES6 - Minutes: ${myCountMin} Every 5: ${dailyCountTotal}`)
+})
+
+
+// create route w/middleware to forward hook to twitter
+app.listen(PORT, () => {
+  //console.log(`Example app listening on port ${PORT}`)
+})
+
+
